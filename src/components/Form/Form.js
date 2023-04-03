963fa5d8695f713c282d6ad1031ca28972ad5881
AdriandGrigore <grigonenicolaeadrian@yahoo.com>
2023-04-03 17:16:44 +0300

    form min date input fixed

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -4,7 +4,7 @@ import "../Form/Form.css"
 const getCurrentDate=()=>{
   const date= new Date()
   const day= date.getDate() < 10 ? `0${date.getDate()}` : date.getDate()
-  const month= date.getMonth()<10 ? `0${date.getMonth()+1}` : date.getMonth()+1
+  const month= date.getMonth() < 10 ? `0${date.getMonth()+1}` : date.getMonth()+1
   const year= date.getFullYear()
   return (
     `${year}-${month}-${day}`
