7f9b2e8670daf3a9abadfd7f4b4050329e9c7df6
AdriandGrigore <grigonenicolaeadrian@yahoo.com>
2023-04-04 13:13:13 +0300

    context added

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import ChefContext from "../src/context/ChefContext"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <StrictMode>
-      <App />
+      <ChefContext>
+        <App />
+      </ChefContext>
     </StrictMode>
   </BrowserRouter>
 );
