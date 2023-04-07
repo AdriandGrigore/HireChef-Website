501c1d259bb86ac3755164809fb734f3b4ecc21e
AdriandGrigore <grigonenicolaeadrian@yahoo.com>
2023-04-07 11:39:40 +0300

    wrapped the app with the redux provider

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { store } from './store/store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <StrictMode>
+      <Provider store={store}>
         <App />
+      </Provider>
     </StrictMode>
   </BrowserRouter>
 );
