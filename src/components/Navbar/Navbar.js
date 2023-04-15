ef76a1bec17c24bfe52afc7b88e0bb0d2b3dd602
AdriandGrigore <grigorenicolaeadrian@yahoo.com>
2023-04-15 11:43:49 +0300

    book a meeting buttons will redirect to login page if user is not logged in

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,18 @@
 import React, {useEffect, useState} from 'react'
-import "../Navbar/Navbar.css"
 import logo from "../../assets/logo.png"
 import {CiHome,CiCircleInfo,CiBookmark} from "react-icons/ci"
 import {RxHamburgerMenu} from "react-icons/rx"
 import {AiOutlineClose,AiOutlineLogin} from "react-icons/ai"
 import {MdOutlineRestaurantMenu} from "react-icons/md"
 import { NavHashLink as Link } from 'react-router-hash-link'
+import useAuth from '../../custom-hooks/useAuth'
+import "../Navbar/Navbar.css"
 
 function Navbar() {
   const [clicked, setClicked]=useState(false)
   const [scrollPosition,setScrollPosition]=useState({position:0, prevPosition:0})
   const isVisible= scrollPosition.position<=scrollPosition.prevPosition
+  const {currentUser} = useAuth()
   
   useEffect(() => {
     const onScroll = () => {
@@ -43,7 +45,7 @@ function Navbar() {
           <Link onClick={()=>setClicked(false)} to="/menu#"><MdOutlineRestaurantMenu/> Menu</Link>
         </li>
         <li>
-          <Link onClick={()=>setClicked(false)} to="/booking#"><CiBookmark/>Book meeting</Link>
+          <Link onClick={()=>setClicked(false)} to={currentUser ? "/booking#" : "/login#"}><CiBookmark/>Book meeting</Link>
         </li>
         <li>
           <Link onClick={()=>setClicked(false)} to="/login#"><AiOutlineLogin/> Log in </Link> 
