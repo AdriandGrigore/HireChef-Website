d1a0d783609b90c755c50a75cb48f6962a619374
AdriandGrigore <grigorenicolaeadrian@yahoo.com>
2023-04-15 12:07:56 +0300

    when loging out, user is redirected to login page

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { signOut } from 'firebase/auth'
 import { auth } from '../../util/firebase-config'
 import useAuth from '../../custom-hooks/useAuth'
+import { useNavigate } from 'react-router-dom'
 import "../UserProfile/UserProfile.css"
 
 function UserProfile() {
   const {currentUser} = useAuth()
-
+  const [logoutError, setLogoutError] = useState()
+  const navigate = useNavigate()
+  
   const logout = async () =>{
-    await signOut(auth)
+    try{
+      await signOut(auth)
+      navigate("/login#")
+    }
+    catch(err){
+      setLogoutError("Something went wrong.Try again")
+    }
   }
 
   return (
     <section className='user-section'>
       <h1>Hello {currentUser.email}</h1>
+      {logoutError ? <small>{logoutError}</small> : null}
       <button onClick={logout}>Log out</button>
     </section>
   )
