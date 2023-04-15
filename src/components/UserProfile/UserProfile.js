31b61afbd90130d34c4157f8191c36700bd34b76
AdriandGrigore <grigorenicolaeadrian@yahoo.com>
2023-04-15 11:57:19 +0300

    UserProfile UI modifed

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { signOut } from 'firebase/auth'
+import { auth } from '../../util/firebase-config'
+import useAuth from '../../custom-hooks/useAuth'
 import "../UserProfile/UserProfile.css"
 
 function UserProfile() {
+  const {currentUser} = useAuth()
+
+  const logout = async () =>{
+    await signOut(auth)
+  }
+
   return (
-    <div>UserProfile</div>
+    <section className='user-section'>
+      <h1>Hello {currentUser.email}</h1>
+      <button onClick={logout}>Log out</button>
+    </section>
   )
 }
 
