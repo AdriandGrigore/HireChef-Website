d33da410b6776544304d5eedbcc07211deb9bb0d
AdriandGrigore <grigorenicolaeadrian@yahoo.com>
2023-04-15 11:14:57 +0300

    implemented authentification

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import loginImg from "../../assets/loginImg.jpg"
 import { NavHashLink as Link } from 'react-router-hash-link'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '../../util/firebase-config'
 import "../LoginForm/LoginForm.css"
 
 function LoginForm() {
+  const [loginEmail, setLoginEmail] = useState("")
+  const [loginPassword, setLoginPassword] = useState("")
+  const [loginError, setLoginError] = useState("")
+
+  const login = async (e) =>{
+    e.preventDefault()
+    
+    try{
+      setLoginError("")
+      await signInWithEmailAndPassword(auth, loginEmail, loginPassword)
+    }
+    catch(err){
+      setLoginError(err.message)
+    }
+  }
+
   return (
     <section className='login-section'>
       <div className='login-card'>
@@ -14,11 +32,22 @@ function LoginForm() {
           <div className='welcome-text'>
             <h1>Welcome</h1>
           </div>
-          <form>
+          <form onSubmit={login}>
+            {loginError ? <small className='login-error'>{loginError}</small> : null}
             <label htmlFor="loginEmail">E-mail</label>
-            <input type='email' id='loginEmail'/>
+            <input 
+              type='email' 
+              id='loginEmail'
+              value={loginEmail}
+              onChange={(e)=>setLoginEmail(e.target.value)}
+            />
             <label htmlFor='loginPassword'>Password</label>
-            <input type='password' id="loginPassword"/>
+            <input 
+              type='password' 
+              id="loginPassword"
+              value={loginPassword}
+              onChange={(e)=>setLoginPassword(e.target.value)}
+            />
             <button type='submit'>Log in</button>
           </form>
           <div className='register-msg'>
--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -1,33 +1,72 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { NavHashLink as Link } from 'react-router-hash-link'
 import signupImg from "../../assets/signupImg.jpg"
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '../../util/firebase-config'
 import "../SignUpForm/SignUpForm.css"
 
 function SignUpForm() {
+  const [signupEmail,setSignUpEmail]  = useState("")
+  const [signupPassword,setSignUpPassword]  = useState("")
+  const [signupConfirmPassword,setSignUpConfirmPassword]  = useState("")
+  const [signupError,setSignUpError]  = useState("")
+  
+  const signUp= async (e) =>{
+    e.preventDefault()
+    
+    if(signupPassword !== signupConfirmPassword){
+      return setSignUpError("Passwords do not match")
+    }
+
+    try{
+      setSignUpError("")
+      await createUserWithEmailAndPassword(auth, signupEmail, signupPassword)
+    }
+    catch(err){
+      setSignUpError(err.message)
+    }
+  }
+
   return (
     <section className='signup-section'>
-    <div className='signup-card'>
-      <div className='signup-img'>
-        <img src={signupImg} alt="signupImg" />
-      </div>
-      <div className='signup-form'>
-        <div className='signup-text'>
-          <h1>Create Account</h1>
+      <div className='signup-card'>
+        <div className='signup-img'>
+          <img src={signupImg} alt="signupImg" />
         </div>
-        <form>
-          <label htmlFor="signupEmail">E-mail</label>
-          <input type='email' id='signupEmail'/>
-          <label htmlFor='signupPassword'>Password</label>
-          <input type='password' id="signupPassword"/>
-          <label htmlFor='signupConfirmPassword'>Confirm Password</label>
-          <input type='password' id="signupConfirmPassword"/>
-          <button type='submit'>Sign up</button>
-        </form>
-        <div className='login-msg'>
-          <small>Already have an account? <Link to="/login#">Login</Link></small>
+        <div className='signup-form'>
+          <div className='signup-text'>
+            <h1>Create Account</h1>
+          </div>
+          <form onSubmit={signUp}>
+            {signupError ? <small className='signup-error'>{signupError}</small> : null}
+            <label htmlFor="signupEmail">E-mail</label>
+            <input 
+              type='email' 
+              id='signupEmail'
+              value={signupEmail}
+              onChange={(e)=>setSignUpEmail(e.target.value)}
+            />
+            <label htmlFor='signupPassword'>Password</label>
+            <input 
+              type='password' 
+              id="signupPassword"
+              value={signupPassword}
+              onChange={(e)=>setSignUpPassword(e.target.value)}
+            />
+            <label htmlFor='signupConfirmPassword'>Confirm Password</label>
+            <input 
+              type='password' 
+              id="signupConfirmPassword"
+              value={signupConfirmPassword}
+              onChange={(e)=>setSignUpConfirmPassword(e.target.value)}
+            />
+            <button type='submit'>Sign up</button>
+          </form>
+          <div className='login-msg'>
+            <small>Already have an account? <Link to="/login#">Login</Link></small>
+          </div>
         </div>
       </div>
-    </div>
   </section>
   )
 }
