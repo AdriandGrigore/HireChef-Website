a6b78c125d387bf8e574fc1abcc20dfe787307e8
AdriandGrigore <grigorenicolaeadrian@yahoo.com>
2023-04-26 17:04:05 +0300

    modal component design changed

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import "../Modal/Modal.css"
 import { useDispatch } from 'react-redux'
 import { closeModal } from '../../features/modalSlice'
-import {BsEnvelopeCheckFill} from "react-icons/bs"
+import { BsEnvelopeCheckFill } from "react-icons/bs"
 import { NavHashLink as Link } from 'react-router-hash-link'
+import "../Modal/Modal.css"
 
 function Modal() {
   const dispatch=useDispatch()  
+
   return (
     <div className='modal-background'>
         <div className='modal-container'>
@@ -15,10 +16,10 @@ function Modal() {
             </div>
             <div className='modal-text'>
                 <h1>The form was submited successfully</h1>
-                <h3>Log into your account to see your meetings</h3>
+                <h3>Press the button below to see your meetings</h3>
             </div>
             <div className='modal-buttons'>
-                <Link>Log in</Link>
+                <Link to="/user/meetings"> Meetings </Link>
                 <button onClick={()=>dispatch(closeModal())}> Go back to page</button>
             </div>
         </div>
