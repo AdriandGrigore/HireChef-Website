a043bc60dd44952721a1cc4477b92ac02a480e44
AdriandGrigore <grigorenicolaeadrian@yahoo.com>
2023-04-26 14:55:25 +0300

    meeting model added

---
+++ src/models/Meeting.js
@@ -0,0 +1,9 @@
+export default class Meeting{
+    constructor(userId,phoneNumber, menu, chef, date){
+        this.userId = userId;
+        this.phoneNumber = phoneNumber;
+        this.menu = menu;
+        this.chef = chef;
+        this.date = date;
+    }
+}
