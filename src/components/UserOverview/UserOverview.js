e1a321068fa797f215f9a5f27228d5aef5a0b3c0
AdriandGrigore <grigorenicolaeadrian@yahoo.com>
2023-05-02 15:16:08 +0300

    total meetings card + upcoming meeting card are changing dynamically

--- src/components/UserOverview/UserOverview.js
+++ src/components/UserOverview/UserOverview.js
@@ -1,16 +1,20 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import "../UserOverview/UserOverview.css"
 
 function UserOverview() {
+  const {userMeetingsList} = useSelector(state=>state.meetings)
+  const upcomingMeeting = userMeetingsList.length > 0 ? userMeetingsList[0].date : "-"
+  
   return (
     <div className='ow-card-container'>
       <div className='ow-card'>
         <p>Total meetings</p>
-        <span>0</span>
+        <span>{userMeetingsList.length}</span>
       </div>
       <div className='ow-card'>
         <p>Upcoming meeting</p>
-        <span>22.04.2023</span>
+        <span>{upcomingMeeting}</span>
       </div>
       <div className='ow-card'>
         <p>Ratings</p>
