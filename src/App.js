339b6c6b0354dc9d4b03e935ad6770e0f9e98030
AdriandGrigore <grigorenicolaeadrian@yahoo.com>
2023-05-02 14:53:06 +0300

    Route added to UserMeetings page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import ProtectedRoute from "./routers/ProtectedRoute";
 import UserBooking from "./pages/UserBooking";
+import UserMeetings from "./pages/UserMeetings";
 import "./App.css"
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path="/menu" element={<Menu/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/signup" element={<SignUp/>}/>
+        <Route path= "/user/meetings" element={<ProtectedRoute> <UserMeetings/> </ProtectedRoute>}/>
         <Route path="/user/booking" element={<ProtectedRoute> <UserBooking/> </ProtectedRoute>}/>
       </Routes>
     </div>
