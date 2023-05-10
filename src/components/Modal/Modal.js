756ced56114c317028d8ea4dd798b0ad4fcaaabb
AdriandGrigore <grigorenicolaeadrian@yahoo.com>
2023-05-10 10:59:32 +0300

    Modal component is now reusable

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,33 +1,18 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { closeModal } from '../../features/modalSlice'
-import { BsEnvelopeCheckFill } from "react-icons/bs"
-import { NavHashLink as Link } from 'react-router-hash-link'
-import { resetForm } from '../../features/bookingFormSlice'
 import "../Modal/Modal.css"
 
-function Modal() {
-    const dispatch=useDispatch()  
-    const {editForm} = useSelector(state => state.bookingForm)
-    
-    const handleClick = () =>{
-        dispatch(closeModal())
-        dispatch(resetForm())
-    }
-
+function Modal({icon, text, buttons}) {
   return (
     <div className='modal-background'>
         <div className='modal-container'>
             <div className='modal-icon'> 
-                <BsEnvelopeCheckFill/>
+                {icon}
             </div>
             <div className='modal-text'>
-                <h1>The form was {editForm.status ? "updated" : "submited"} successfully</h1>
-                <h3>Press the button below to see your meetings</h3>
+                {text}
             </div>
             <div className='modal-buttons'>
-                <Link onClick={handleClick} to="/user/meetings#"> Meetings </Link>
-                <button onClick={handleClick}> Go back to page</button>
+                {buttons}  
             </div>
         </div>
     </div>
