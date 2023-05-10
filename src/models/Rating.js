7c3920f0011557a73f3b4568ce44f3203b7d3429
AdriandGrigore <grigorenicolaeadrian@yahoo.com>
2023-05-10 16:15:28 +0300

    Rating model + firestore rating collection implemented

---
+++ src/models/Rating.js
@@ -0,0 +1,8 @@
+export default class Rating {
+    constructor(id, chef, comment, rating){
+        this.userId = id
+        this.chef = chef
+        this.comment = comment
+        this.rating =  rating 
+    }
+}
