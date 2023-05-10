cb5a9e1036005b90d9a036cac7671cb8aebb1e09
AdriandGrigore <grigorenicolaeadrian@yahoo.com>
2023-05-10 12:13:06 +0300

    Modal component implemented on UserMeeting page

--- src/pages/UserMeetings.js
+++ src/pages/UserMeetings.js
@@ -3,13 +3,39 @@ import Navbar from "../components/Navbar/Navbar"
 import Footer from "../components/Footer/Footer"
 import MeetingList from "../components/MeetingList/MeetingList"
 import RatingModal from '../components/RatingModal/RatingModal'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import {FiDelete} from "react-icons/fi"
+import Modal from '../components/Modal/Modal'
+import { Link } from 'react-router-dom'
+import { deleteMeeting } from '../features/meetingSlice'
+import { closeModal } from '../features/modalSlice'
 
 function UserMeetings() {
   const {isRatingModalOpen} = useSelector(state => state.ratingModal)
-  
+  const {isModalOpen} = useSelector(state => state.modal)
+  const dispatch = useDispatch()
+
   return (
     <>
+      {isModalOpen ? 
+        <Modal 
+          icon={<FiDelete style={{color:"red"}}/>}  
+          text={     
+            <>
+              <h1>Are you sure you want to delete?</h1>
+              <h3>You can't undo this action</h3>
+            </>
+          }
+          buttons={
+            <>
+              <Link className="delete-link" onClick={() => dispatch(deleteMeeting())}> Delete </Link>
+              <button onClick={() => dispatch(closeModal())}> Go back to page</button>
+            </>
+          }
+        /> 
+        : 
+        null 
+      }
       {isRatingModalOpen ? <RatingModal/> : null}
       <Navbar/>
       <MeetingList/>
