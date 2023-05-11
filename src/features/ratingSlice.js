81716e3488f76cf934aec2bff37b3ec7de86a6b5
AdriandGrigore <grigorenicolaeadrian@yahoo.com>
2023-05-11 10:39:45 +0300

    fetchRatings thunk implemented

--- src/features/ratingSlice.js
+++ src/features/ratingSlice.js
@@ -1,9 +1,30 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { getDocs } from "firebase/firestore";
+import { ratingsCollectionRef } from "../util/firebase-config";
+
 
 const initialState={
     isRatingModalOpen: false,
     chefSelectedForRating: "",
+    userRatingsList: [],
+    userRatingsLoading: false,
+    userRatingsError: false,
 }
+
+export const fetchRatings = createAsyncThunk("firestore/fetchRatings", async (loggedInUser) =>{
+    const res = await getDocs(ratingsCollectionRef)
+    const data = res.docs
+        .map(doc => {
+            const ratingData = doc.data()
+            return ({
+                ...ratingData, 
+                ratingId: doc.id,
+            })
+        })
+        .filter(rating => rating.userId === loggedInUser.uid)
+    
+    return data
+})
 
 const ratingSlice= createSlice({
     name:"modal",
@@ -16,6 +37,20 @@ const ratingSlice= createSlice({
             state.isRatingModalOpen= true
             state.chefSelectedForRating = payload
         }
+    },
+    extraReducers: (builder) => {
+        builder.addCase(fetchRatings.pending, (state) =>{
+            state.userRatingsLoading = true
+        })
+        builder.addCase(fetchRatings.fulfilled, (state, {payload}) =>{
+            state.userRatingsLoading= false
+            state.userRatingsList= payload
+        })
+        builder.addCase(fetchRatings.rejected, (state, action) =>{
+            state.userRatingsLoading= false
+            state.userRatingsError = true
+            console.log(action.error.message)
+        })
     }
 })
 
