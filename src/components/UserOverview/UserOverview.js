b35a3fbaa3ed5c2e91d169b68ad83fc1d8715948
AdriandGrigore <grigorenicolaeadrian@yahoo.com>
2023-05-16 11:42:34 +0300

    UserOvierview now shows the ratings number + useEffect optimized

--- src/components/UserOverview/UserOverview.js
+++ src/components/UserOverview/UserOverview.js
@@ -1,20 +1,34 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchMeetings } from '../../features/meetingSlice'
+import {fetchRatings} from '../../features/ratingSlice'
 import useAuth from '../../custom-hooks/useAuth'
 import { convertDateFormat } from '../MeetingList/MeetingList'
 import "../UserOverview/UserOverview.css"
 
 function UserOverview() {
-  const {userMeetingsList} = useSelector(state=>state.meetings)
+  const {userMeetingsList, userMeetingsLoading} = useSelector(state=>state.meetings)
   const filteredMeetingDates = userMeetingsList.filter(meeting => new Date(convertDateFormat(meeting.date)) > new Date())
   const upcomingMeeting = filteredMeetingDates.length > 0 ? filteredMeetingDates[0].date : "-"
+  const {userRatingsList, userRatingsLoading} = useSelector(state => state.rating)
   const {loggedInUser} = useAuth()
   const dispatch = useDispatch()
 
-  useEffect(()=>{
-    dispatch(fetchMeetings(loggedInUser))
-  },[dispatch, loggedInUser])
+  useEffect(() => {
+    //fetching data (meetings) if it hasn't been fetched before
+    if(userMeetingsList.length === 0 && !userMeetingsLoading){
+      console.log("meetings fetched")
+      dispatch(fetchMeetings(loggedInUser))
+    }
+  },[dispatch, loggedInUser, userMeetingsList.length, userMeetingsLoading])
+
+  useEffect(() => {
+    //fetching data (ratings) if it hasn't been fetched before
+    if(userRatingsList.length === 0 && !userRatingsLoading){
+      dispatch(fetchRatings(loggedInUser))
+      console.log("fetching ratings")
+    }
+  }, [dispatch, loggedInUser, userRatingsList.length, userRatingsLoading])
 
   return (
     <div className='ow-card-container'>
@@ -28,7 +42,7 @@ function UserOverview() {
       </div>
       <div className='ow-card'>
         <p>Ratings</p>
-        <span>0</span>
+        <span>{userRatingsList.length}</span>
       </div>
     </div>
   )
